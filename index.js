e902f0dd4f83dd2f85a1342bb16191c9f899df50
Denise Ignatova <denis.ignatova@re-flow.co.uk>
2021-03-23 12:47:36 +0000

    field helpers

---
+++ index.js
@@ -0,0 +1,38 @@
+  exports.nameGenerator =  (str, slices, idKey) => {
+        let regex = str.replace(/[\d,&,-/_?():.]/g,"").toLowerCase();
+        let strArray = regex.split(' ');
+        let idModifier = idKey ?? 'Id';
+        if (strArray.length > 1) {
+            if (slices.length > 0) {
+                switch(slices.length) {
+                    case 1:
+                        return strArray[slices[0]+idModifier];
+                        break;
+                    case 2:
+                        return strArray[slices[0]].concat(strArray[slices[1]].charAt(0).toUpperCase()+strArray[slices[1]].slice(1))+idModifier;
+                        break;
+                    case 3:
+                        return strArray[slices[0]].concat(strArray[slices[1]].charAt(0).toUpperCase()+strArray[slices[1]].slice(1)+strArray[slices[2]].charAt(0).toUpperCase()+strArray[slices[2]].slice(1))+idModifier;
+                        break;
+                    case 4:
+                        return strArray[slices[0]].concat(strArray[slices[1]].charAt(0).toUpperCase()+strArray[slices[1]].slice(1)+strArray[slices[2]].charAt(0).toUpperCase()+strArray[slices[2]].slice(1)+strArray[slices[3]].charAt(0).toUpperCase()+strArray[slices[3]].slice(1))+idModifier;
+                        break;
+
+                }
+            } else {
+                return strArray[0].concat(strArray[1].charAt(0).toUpperCase()+strArray[1].slice(1))+idModifier;
+            }
+        } else {
+            return strArray[0]+idModifier;
+        }
+
+    }
+
+    exports.uniqueFieldName = (items)  => {
+        if (items.length > 1) {
+            let arrayOfNames = items.map((item) => item.name);
+            arrayOfNames = arrayOfNames.filter((item) => item !== "");
+            return arrayOfNames.some((item, index) => arrayOfNames.indexOf(item) !== index)
+        }
+    }
+
